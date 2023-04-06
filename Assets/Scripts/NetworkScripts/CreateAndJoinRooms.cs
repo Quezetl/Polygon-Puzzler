2e20bfe3fecc7324282bcc88c3b976704353be8e
Quezetl <bradleyberning@yahoo.com>
2023-04-06 13:34:24 -0700

    [Implementation]: Added connecting to rooms/lobbies

---
+++ Assets/Scripts/NetworkScripts/CreateAndJoinRooms.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
+{
+    public TMPro.TMP_InputField createInput;
+    public TMPro.TMP_InputField joinInput;
+
+
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(createInput.text);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Demo1");
+    }
+}
