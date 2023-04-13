6988241f3465ae3cdf40815deb4dd3b37b8ca27c
Quezetl <bradleyberning@yahoo.com>
2023-04-13 13:49:47 -0700

    [Sprite Render]: fixed RPC for sprite rendering

--- Assets/Scripts/SquareGrow.cs
+++ Assets/Scripts/SquareGrow.cs
@@ -4,12 +4,12 @@ using System.Runtime.CompilerServices;
 
 public class SquareGrow : MonoBehaviour
 {
-    public float growthRate = 2f; 
+    public float growthRate = 2f;
     public Transform top;
     public Transform bottom;
     public LayerMask lMask;
     public SpriteRenderer sprite;
-    private Vector2 initialSize; 
+    private Vector2 initialSize;
     private bool canGrow;
     private bool flip;
     PhotonView view;
@@ -35,48 +35,56 @@ public class SquareGrow : MonoBehaviour
 
     void FixedUpdate()
     {
-        
-        top.localPosition = new Vector3(0, sprite.size.y / 2 , 0);
-        bottom.localPosition = new Vector3(0, -sprite.size.y / 2 , 0);
-        
+
+        top.localPosition = new Vector3(0, sprite.size.y / 2, 0);
+        bottom.localPosition = new Vector3(0, -sprite.size.y / 2, 0);
+
         if (Physics2D.OverlapBox(top.position, new Vector2(1.9f, 0.1f), transform.eulerAngles.z, lMask) && Physics2D.OverlapBox(bottom.position, new Vector2(1.9f, 0.1f), transform.eulerAngles.z, lMask))
         {
-            canGrow  = false;
+            canGrow = false;
         }
         else
         {
             canGrow = true;
         }
-            
 
+        if (!view.IsMine)
+            return;
         if (Input.GetKey(KeyCode.LeftControl))
         {
-            view.RPC("shapeChange", RpcTarget.All);
+            if (flip)
+            {
+                if (!canGrow)
+                    return;
+
+                view.RPC("shapeChangeGrow", RpcTarget.AllBuffered, null);
+            }
+            else if(!flip)
+            {
+                view.RPC("shapeChangeShrink", RpcTarget.AllBuffered, null);
+            }
         }
     }
 
     [PunRPC]
-    private void shapeChange()
+    private void shapeChangeShrink()
     {
-        if (flip)
-        {
-            if (canGrow)
-            {
-                Debug.Log("Growing");
-                Vector2 newSize = sprite.size;
-                newSize.y += growthRate * Time.deltaTime;
-                sprite.size = newSize;
-            }
-        }
-        else if (!flip)
-        {
-            Debug.Log("Shrinking");
-            Vector2 newSize = sprite.size;
-            if (newSize.y < 2)
-                return;
-            newSize.y -= growthRate * Time.deltaTime;
-            sprite.size = newSize;
-        }
+        Debug.Log("Shrinking");
+        Vector2 newSize = sprite.size;
+        if (newSize.y < 2)
+            return;
+        newSize.y -= growthRate * Time.deltaTime;
+        sprite.size = newSize;
+    }
+
+
+    [PunRPC]
+    private void shapeChangeGrow()
+    {
+        Debug.Log("Growing");
+        Vector2 newSize = sprite.size;
+        newSize.y += growthRate * Time.deltaTime;
+        sprite.size = newSize;
     }
 }
 
